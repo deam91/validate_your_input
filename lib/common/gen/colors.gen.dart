48aa84ce09a1381c9f0f81f7d0ca53b8004c2dd3
Deam <deam@aleph.engineering>
2022-06-25 20:08:19 -0400

    Gen assets and colors

---
+++ lib/common/gen/colors.gen.dart
@@ -0,0 +1,42 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// Color: #F6F6F6
+  static const Color gray01 = Color(0xFFF6F6F6);
+
+  /// Color: #E8E8E8
+  static const Color gray02 = Color(0xFFE8E8E8);
+
+  /// Color: #BDBDBD
+  static const Color gray03 = Color(0xFFBDBDBD);
+
+  /// Color: #666666
+  static const Color gray04 = Color(0xFF666666);
+
+  /// Color: #5DB075
+  static const Color primary = Color(0xFF5DB075);
+
+  /// Color: #4B9460
+  static const Color secondary = Color(0xFF4B9460);
+
+  /// Color: #101010
+  static const Color secondaryBlack = Color(0xFF101010);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+}
